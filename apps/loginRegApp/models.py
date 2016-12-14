6585143338e2845587feefac9180468973eb171f
timlichen <tim.li.chen@gmail.com>
2016-12-14 16:16:52 -0500

    registration complete

--- apps/loginRegApp/models.py
+++ apps/loginRegApp/models.py
@@ -1,5 +1,41 @@
 from __future__ import unicode_literals
-
+import re, bcrypt
 from django.db import models
 
 # Create your models here.
+class userManager(models.Manager):
+    def validate(self, postData):
+        flag = False
+        errors = {}
+        # info is not blank
+        if not postData['f_name'] or not postData['l_name'] or not postData['email'] or not postData['password'] or not postData['c_password']:
+            flag = True
+            errors['message'] = "All fields must be filled in."
+        # passwords match
+        if postData['password'] != postData['c_password']:
+            errors['message_2'] = "Your passwords don't match."
+        # validate email
+        if not re.match(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$', postData['email']):
+            flag = True
+            errors['message_3'] = "Please enter a valid email."
+
+        if flag:
+            return (flag, errors)
+        else:
+            hashed = bcrypt.hashpw(postData['password'].encode('utf-8'), bcrypt.gensalt())
+
+            # Data is clean, ready to be written to database
+            User.objects.create(first_name = postData['f_name'], last_name = postData['l_name'], email = postData['email'], password = hashed)
+
+            return(flag, "gratz")
+
+class User(models.Model):
+    first_name = models.CharField(max_length = 55)
+    last_name = models.CharField(max_length = 55)
+    email = models.CharField(max_length = 55)
+    password = models.CharField(max_length = 100)
+
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now_add = True)
+
+    objects = userManager()
--- apps/loginRegApp/views.py
+++ apps/loginRegApp/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render, redirect
+from .models import User
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
+    print User.objects.last().id
     return render(request, 'loginRegApp/index.html')
 def register(request):
-    print 'yes'
+    postData = {
+        'f_name': request.POST['first_name'],
+        'l_name': request.POST['last_name'],
+        'email': request.POST['email'],
+        'password': request.POST['password'],
+        'c_password': request.POST['confirm_password']
+    }
+
+    user = User.objects.validate(postData)
+
+    if user[0]:
+        for error in user[1].itervalues():
+            messages.error(request, error)
+    else:
+        request.session['id'] = User.objects.last().id
+        user = User.objects.last()
+        context = {'user': user}
+        return render(request, "loginRegApp/loggedin.html", context)
+
     return redirect('/')
